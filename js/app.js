2e89bff3657bb92ca9f3973d5ded197e7a0732c7
Richard Lipa <rlipac31@gmail.com>
2018-09-20 01:38:04 -0500

    subiendo my-consorcio

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    $("#boton").click(function () {
+        $("#menu-generos").toggle(900);
+    });
+    $("#boton1").click(function () {
+        $("#menu-generos1").toggle(900);
+    });
+    $("#boton-2").click(function () {
+        $("#menu-generos-2").toggle(900);
+    });
+    $("#boton-3").click(function () {
+        $("#menu-generos-3").toggle(900);
+    });
+    $("#boton4").click(function () {
+        $("#menu-generos4").toggle(900);
+    });
+    $("#boton-5").click(function () {
+        $("#menu-generos-5").toggle(900);
+    });
+    $("#boton-6").click(function () {
+        $("#menu-generos-6").toggle(900);
+    });
+    $("#boton-8").click(function () {
+        $("#menu-generos-8").toggle(900);
+    });
+    $("#boton-9").click(function () {
+        $("#menu-generos-9").toggle(900);
+    });
+    $("#boton10").click(function () {
+        $("#menu-generos10").toggle(900);
+    });
+    $("#boton11").click(function () {
+        $("#menu-generos11").toggle(900);
+    });
+    $("#boton12").click(function () {
+        $("#menu-generos12").toggle(900);
+    });
+
+});
+
